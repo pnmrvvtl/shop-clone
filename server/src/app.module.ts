b0af7179123be011ef290695e547a65755da3bfd
pnmrvvtl <pnmrv.vtl@gmail.com>
2023-06-25 21:48:20 +0300

    feat: add orders resource

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,13 +5,18 @@ import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ProductsModule } from './products/products.module';
 import { OrdersModule } from './orders/orders.module';
-import { OrderItemsModule } from './order-items/order-items.module';
 import { CartsModule } from './carts/carts.module';
-import { CartItemsModule } from './cart-items/cart-items.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, PrismaModule, ProductsModule, OrdersModule, OrderItemsModule, CartsModule, CartItemsModule, AuthModule],
+  imports: [
+    UsersModule,
+    PrismaModule,
+    ProductsModule,
+    OrdersModule,
+    CartsModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
